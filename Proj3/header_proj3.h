f536c09bce53362e62e49d74ca4cf7192e3556a2
Caio <78882269+lordvitamina@users.noreply.github.com>
2022-03-15 10:33:32 -0300

    Add files via upload

---
+++ Proj3/header_proj3.h
@@ -0,0 +1,5 @@
+//header
+int cadastro(int *vet, int n);
+int exibir_n(int *vet, int n);
+int exibir_i(int*vet, int n);
+int remover(int *vet);
---
+++ Proj3/proj_3cpp.cpp
@@ -0,0 +1,36 @@
+//cpp funções
+//Desenvolver as funções da biblioteca
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h> //necessário para usar as funções malloc() e free()
+#include <conio.h>
+#include <locale.h>
+
+ int i=0;
+int cadastro(int *vet, int n){
+	for(int i=0;i<n;i++){
+		printf("Posição [%d] vetor: ",i+1);
+		scanf("%d",&vet[i]);
+	}
+}
+
+int exibir_n(int *vet, int n){
+	for(int i=0;i<n;i++){
+		printf("%d ",vet[i]);
+	}
+}
+
+int exibir_i(int *vet, int n){
+	for(int i=0;i<n;i++){
+		printf("%d ",i+1);
+	}
+}
+
+
+int remover(int *vet, int n){
+	int n_novo=n-1;
+	
+	for(int i=0;i<n_novo;i++){
+		printf("%d\n",vet[i]);
+}
+}
